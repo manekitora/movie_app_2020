e40e6a5e912db7cb134f3d52d8521380eee39c0a
manekitora <manekitora@protonmail.com>
2020-11-09 23:26:12 +0900

    포스터까지

--- src/App.js
+++ src/App.js
@@ -39,40 +39,35 @@ class App extends React.Component {
     movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
-  };
 
 
 
 
   render() {
     const { isLoading, movies } = this.state;
-    return (<div>
-      {isLoading
-        ? 'Loading...'
-        : movies.map((movie) => {
-          return (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
 
-            />
-          );
-        })}
-    </div>
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+
+                />
+              ))}
+            </div>
+          )}
+      </section>
     );
 
   }
