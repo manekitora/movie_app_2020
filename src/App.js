e12d82aabb60a5565e647c7bab0c53d4cb9c89c8
manekitora <manekitora@protonmail.com>
2020-11-11 20:40:07 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,78 +1,21 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
 import './App.css';
+import { func } from 'prop-types';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating'); // axios로 api 호출
-    this.setState({ movies, isLoading: false });
-  }
-
-
-  componentDidMount() {
-    // 영화 데이터 로딩!
-    this.getMovies();
-
-  }
-
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
-  }
-
-
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-
-
-
-
-  render() {
-    const { isLoading, movies } = this.state;
-
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )}
-      </section>
-    );
-
-  }
-
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
 }
 
-export default App;
+export default App; 
